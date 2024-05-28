63dbd0149c6e29c3683b5096915238b45d42152e
Ayodeji Marvellous Olugbamila <olugbamilamarvellous@gmail.com>
2024-05-28 16:07:11 +0100

    change

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 5000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-//app.get('/', (req, res) => res.send('THIS IS CSC 314  CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS THIS PAGE INDICATES SUCCESS AYODEJI MARVELLOUS OLUGBAMILA  VUG/CSC/22/8016'));
+//app.get('/', (req, res) => 
+//res.send('THIS IS CSC 314  CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS THIS PAGE INDICATES SUCCESS AYODEJI MARVELLOUS OLUGBAMILA  VUG/CSC/22/8016')
+//);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
